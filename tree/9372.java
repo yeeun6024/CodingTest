4da52186e12a3bd434870fc8ea4964b27e1cc31f
yeeun6024 <77540520+yeeun6024@users.noreply.github.com>
2021-04-19 17:28:07 +0900

    Create 9372.java

---
+++ tree/9372.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int t = sc.nextInt();
+		
+		while(t>0) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			
+			for(int i=0; i<m; i++) {
+				int x = sc.nextInt();
+				int y = sc.nextInt();
+			}
+			
+			sb.append(n-1+"\n");
+			t--;
+			
+		}
+		
+		System.out.println(sb);
+	}
+
+}
