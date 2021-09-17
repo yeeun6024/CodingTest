6acd80d0e5e4d860860db7c598c744876681d5cc
yeeun6024 <77540520+yeeun6024@users.noreply.github.com>
2021-09-17 14:10:04 +0900

    Create WordTransfer.py

---
+++ dfs/WordTransfer.py
@@ -0,0 +1,39 @@
+// 프로그래머스
+// 단어변환
+// https://programmers.co.kr/learn/courses/30/lessons/43163?language=python3#
+  
+// 210917
+
+answer = 50
+
+def solution(begin, target, words):
+    global answer
+    
+    dfs(begin, target, words, len(words))
+    
+    if(answer == 50):
+        answer = 0
+    
+    return answer
+
+def dfs(begin, target, words, length):
+    global answer
+    
+    if(begin==target):
+        if(answer > length-len(words)):
+            answer = length-len(words)
+            return
+    
+    if(len(words)==0):
+        return
+    
+    for word in words:
+        count = 0
+        for i in range(len(word)):
+            if begin[i]==word[i]: 
+                count += 1
+            
+        if(count >= len(word)-1):
+            words.remove(word)
+            dfs(word, target, words, length)
+            words.append(word)
