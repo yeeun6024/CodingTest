1635563ff484a52a86e0d118e5d1c76652038024
yeeun6024 <77540520+yeeun6024@users.noreply.github.com>
2021-09-26 01:41:48 +0900

    Update 완주하지못한선수.py

--- programmers/hash/완주하지못한선수.py
+++ programmers/hash/완주하지못한선수.py
@@ -1,8 +1,8 @@
-// 프로그래머스
-// 완주하지 못한 선수
-// https://programmers.co.kr/learn/courses/30/lessons/42576
+# 프로그래머스
+# 완주하지 못한 선수
+# https://programmers.co.kr/learn/courses/30/lessons/42576
   
-// 21/09/26
+# 21/09/26
 
 
 def solution(participant, completion):
