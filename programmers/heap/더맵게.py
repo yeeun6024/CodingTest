4dc5720f9d25bf407a4615450a16994c998c6cee
yeeun6024 <77540520+yeeun6024@users.noreply.github.com>
2021-10-03 02:50:56 +0900

    Create 더맵게.py

---
+++ programmers/heap/더맵게.py
@@ -0,0 +1,25 @@
+# 프로그래머스
+# 더 맵게
+# https://programmers.co.kr/learn/courses/30/lessons/42626?language=python3
+
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    
+    heapq.heapify(scoville)
+    
+    while True:
+        if scoville[0] >= K or len(scoville)==1:
+            break
+        
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        new = a + b * 2
+        heapq.heappush(scoville, new)
+        answer += 1
+        
+    if scoville[0] < K:
+        answer = -1
+
+    return answer
