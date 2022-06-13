fafd3b8cd7d5e58b0aabf1d471183c898852fe4f
yeeun6024 <77540520+yeeun6024@users.noreply.github.com>
2022-06-13 22:21:32 +0900

    Create 2751.py

---
+++ sorting/2751.py
@@ -0,0 +1,16 @@
+# 수 정렬하기 2
+# https://www.acmicpc.net/problem/2751
+
+
+import sys
+
+n = int(input())
+num = []
+
+for i in range(n):
+    num.append(int(sys.stdin.readline())) # 입력은 input()보다 이게 빠름
+
+num.sort()  # sort()는 O(nlogn)
+
+for i in num:
+    print(i)
